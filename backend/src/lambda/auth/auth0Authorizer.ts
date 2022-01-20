d6c704ce75f0a90a4cf6165669ed2ffbf7ae8572
Steph <stepsen.dev@gmail.com>
2022-01-20 13:46:19 +0000

    code clean up

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,15 +1,14 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-
-import { decode, verify } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
+import { decode, verify } from 'jsonwebtoken'
+
+import { createLogger } from '../../utils/logger'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// json web key url
 const jwksUrl = 'https://dev-b8waqqsd.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
@@ -18,7 +17,7 @@ export const handler = async (
   logger.info('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
-    logger.info('User was authorized', jwtToken)
+    logger.info('User was authorized')
 
     return {
       principalId: jwtToken.sub,
@@ -56,26 +55,9 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  console.log(jwt)
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-
   let key = await getSigningKey(jwksUrl, jwt.header.kid)
-  console.log('KEY', key)
-  // const response = await Axios(jwksUrl)
-  // const responseData = response.data
-  // const signingKey = responseData['keys'].find(
-  //   (key) => key['kid'] === jwt['header']['kid']
-  // )
-  // if (!signingKey) {
-  //   throw new Error('Invalid Signing key')
-  // }
-  // return verify(token, certToPEM(signingKey), {
-  //   algorithms: ['RS256']
-  // }) as JwtPayload
+
   return verify(token, key.publicKey, { algorithms: ['RS256'] }) as JwtPayload
-  // return verify(token, jwksUrl, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -114,8 +96,8 @@ const getSigningKey = async (jwkurl, kid) => {
     })
   const signingKey = signingKeys.find((key) => key.kid === kid)
   if (!signingKey) {
+    logger.error('No signing keys found')
     throw new Error('Invalid signing keys')
-    // logger.error('No signing keys found')
   }
   logger.info('Signing keys created successfully ', signingKey)
   return signingKey
