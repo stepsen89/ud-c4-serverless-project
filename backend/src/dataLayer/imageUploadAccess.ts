269643141805571961154e80b7d29f611c616f45
Steph <stepsen.dev@gmail.com>
2022-01-20 13:28:31 +0000

    enable xray tracing

--- backend/src/dataLayer/imageUploadAccess.ts
+++ backend/src/dataLayer/imageUploadAccess.ts
@@ -1,4 +1,7 @@
 import * as AWS from 'aws-sdk'
+const AWSXRay = require('aws-xray-sdk')
+
+const XAWS = AWSXRay.captureAWS(AWS)
 
 import { createLogger } from '../utils/logger'
 
@@ -6,7 +9,7 @@ const logger = createLogger('TodosAccess')
 
 export class ImageUploadAccess {
   constructor(
-    private readonly s3Client = new AWS.S3({ signatureVersion: 'v4' }),
+    private readonly s3Client = new XAWS.S3({ signatureVersion: 'v4' }),
     private readonly bucket = process.env.ATTACHMENT_S3_BUCKET,
     private readonly expirationTime = Number(process.env.SIGNED_URL_EXPIRATION)
   ) {}
